e6bccd7cf24d6fb3913f7f73c72e591435a3e1d6
Parinda Dear <142294668+ParindaDear@users.noreply.github.com>
2025-08-15 16:34:07 +0700

    Update 15 Aug 2025

---
+++ TimesTablePathVar.js
@@ -0,0 +1,16 @@
+const http = require('node:http');
+const server = http.createServer((req, res) => {
+    const urlObj = new URL(req.url, `http://${req.headers.host}`);
+    const pathVar = urlObj.pathname.split('/').filter(Boolean);
+    number = pathVar[1];
+
+    res.write(`Multiplication of ${number}:\n`);
+    for (let i = 1; i <= 12; i++) {
+        res.write(number + ' x ' + i + ' = ' + i * number + '\n');
+    }
+    res.end();
+});
+
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000');
+});
